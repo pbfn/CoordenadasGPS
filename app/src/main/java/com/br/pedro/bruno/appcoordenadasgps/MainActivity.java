616b9d82f87ae0cca229c00a61322bbf5c30caf3
Pedro Bruno <pbruno1283@gmail.com>
2021-06-06 10:49:26 -0300

    Coordenada Salva com sucesso

--- app/src/main/java/com/br/pedro/bruno/appcoordenadasgps/MainActivity.java
+++ app/src/main/java/com/br/pedro/bruno/appcoordenadasgps/MainActivity.java
@@ -1,16 +1,20 @@
 package com.br.pedro.bruno.appcoordenadasgps;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -71,7 +75,9 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //abrir caixa para aceitar as permissões
     private boolean solicitarPermissaoParaObterLocalizacao() {
+
         Toast.makeText(this, "Aplicativo não tem permissão", Toast.LENGTH_LONG).show();
         List<String> permissoesNegadas = new ArrayList<>();
         int permissaoNegada;
@@ -84,18 +90,42 @@ public class MainActivity extends AppCompatActivity {
             }
 
         }
-        
-        return true;
+        if(!permissoesNegadas.isEmpty()){
+            ActivityCompat.requestPermissions(
+                    MainActivity.this,
+                    permissoesNegadas.toArray(new String[permissoesNegadas.size()]),
+                    APP_PERMISSOES_ID);
+            return false;
+        }else{
+
+            return true;
+        }
+
     }
 
     private void capturarUltimaLocalizacaoValida() {
 
-        latitude = 1.95;
-        longitude = 2.57;
+        @SuppressLint("MissingPermission")
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-        txtValorLatitude.setText(String.valueOf(latitude));
-        txtValorLongitude.setText(String.valueOf(longitude));
+        if(location !=null){
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+        }else{
+            latitude = 0.00;
+            longitude = 0.00;
+        }
+
+        txtValorLatitude.setText(formataGeopoit(latitude));
+        txtValorLongitude.setText(formataGeopoit(longitude));
 
         Toast.makeText(this, "Coordenadas obtidas com sucesso", Toast.LENGTH_LONG).show();
     }
+
+    private String formataGeopoit(double valor){
+        DecimalFormat decimalFormat = new DecimalFormat("#.######");
+        return  decimalFormat.format(valor);
+    }
+
+
 }
